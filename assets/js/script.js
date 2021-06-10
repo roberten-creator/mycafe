dea3002a952a36d5a383afc80edcab559defd9b6
roberten-creator <80085689+roberten-creator@users.noreply.github.com>
2021-06-10 23:40:50 +0900

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+
+
+$(function() {
+  var imgHeight = $('.kv-wrapper').outerHeight();
+  var bgHeight = $('.fv').outerHeight();
+
+  $(".burger-btn").on('click', function(){
+    if( $(window).scrollTop() < imgHeight -50){
+
+      $('.bar').toggleClass('cross');
+      $('.header-nav').toggleClass('open');
+      $('.burger-musk').fadeToggle(300);
+      $('body').toggleClass('noscroll');
+    }else {
+
+      $(this).toggleClass('black');
+      $('.bar').toggleClass('cross');
+      $('.header-nav').toggleClass('open');
+      $('.burger-musk').fadeToggle(300);
+      $('body').toggleClass('noscroll');
+    }
+  });
+
+  $(window).on('load scroll', function() {
+    if ($(window).scrollTop() < imgHeight -50) {
+      $('.site-title').removeClass('black');
+    }else {
+      $('.site-title').addClass('black');
+    }
+
+    if ($(window).scrollTop() < bgHeight -50) {
+      $('.burger-btn').removeClass('black');
+    }else {
+      $('.burger-btn').addClass('black');
+    }
+  });
+
+  $(".nav-item").on('click', function(){
+    $('.bar').removeClass('cross');
+    $('.header-nav').removeClass('open');
+    $('.burger-musk').fadeToggle(300);
+    $('body').removeClass('noscroll');
+  })
+
+
+});
